42cb3313ae88fce75fd787bd4991efa03d009145
Christos Georgiou <xris.georgiou@gmail.com>
2015-04-05 13:34:09 +0300

    top menu cotnacts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,127 +3,174 @@
 angular.module('balance', ['ui.router'])
 
 .run(
-	['$rootScope', '$state', '$stateParams',
-		function($rootScope, $state, $stateParams) {
-			$rootScope.$state = $state;
-			$rootScope.$stateParams = $stateParams;
-		}
-	]
+    function($rootScope, $state, $stateParams, $fakedb) {
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+        $rootScope.$fakedb = $fakedb;
+    }
+
 )
+
+// Sexy directive for semantic-ui-dropdown
+.directive('dropdown', function() {
+    return {
+        restrict: 'C',
+        link: function(scope, element, attrs) {
+            $(element).dropdown();
+        }
+    };
+})
+
+.directive('megamenu', function() {
+    return {
+        restrict: 'C',
+        link: function(scope, element, attrs) {
+            $(element).popup({
+                inline: true,
+                exclusive: true,
+                movePopup: false,
+                preserve: true,
+                on: "click",
+                transition: 'fly down',
+                duration: 750,
+            });
+        }
+    };
+})
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-	$urlRouterProvider.otherwise("/dashboard");
+    $urlRouterProvider.otherwise("/login");
 
-	$stateProvider
+    $stateProvider
 
-		.state('app', {
-			abstract: true,
-			templateUrl: "views/_layouts/main.html",
-			controller: "AppCtrl"
-		})
-		.state('app.dashboard', {
-			url: '/dashboard',
-			templateUrl: "views/dashboard/index.html",
-			controller: "DashboardCtrl"
-		})
 
-	.state('app.contacts', {
-			url: "/contacts",
-			abstract: true,
-			templateUrl: "views/contacts/_layout.html",
-			controller: "ContactsCtrl",
-		})
-		.state('app.contacts.empty', {
-			url: "/",
-			templateUrl: "views/contacts/empty.html",
-			controller: function($state,$fakedb){
+        .state('login', {
+        url: '/login',
+        templateUrl: "views/login/login.html",
+        controller: "LoginCtrl"
+    })
 
-					$fakedb.companies().then(function(companies) {
-						var _company = companies[0];
-						$state.go("app.contacts.details",{alias:_company.alias});
-					})
 
-			},
-		})
-		.state('app.contacts.details', {
-			url: "/:alias",
-			templateUrl: "views/contacts/details.html",
-		})
+    .state('app', {
+            abstract: true,
+            templateUrl: "views/_layouts/main.html",
+            controller: "AppCtrl"
+        })
+        .state('app.dashboard', {
+            url: '/dashboard',
+            templateUrl: "views/dashboard/index.html",
+            controller: "DashboardCtrl"
+        })
 
-	.state('app.financial', {
-			url: "/financial",
-			abstract: true,
-			templateUrl: "views/financial/_layout.html",
-			controller: "FinancialCtrl"
-		})
-		.state('app.financial.dashboard', {
-			url: "/",
-			templateUrl: "views/financial/dashboard.html",
-		})
-		.state('app.financial.details', {
-			url: "/{:fid}",
-			templateUrl: "views/financial/details.html",
-		})
+    .state('app.contacts', {
+            url: "/contacts",
+            abstract: true,
+            templateUrl: "views/contacts/_layout.html",
+            controller: "ContactsCtrl",
+        })
+        .state('app.contacts.empty', {
+            url: "/",
+            templateUrl: "views/contacts/empty.html",
+            controller: function($state, $fakedb) {
+
+                $fakedb.companies().then(function(companies) {
+                    var _company = companies[0];
+                    $state.go("app.contacts.details", {
+                        alias: _company.alias
+                    });
+                })
+
+            },
+        })
+        .state('app.contacts.details', {
+            url: "/:alias",
+            templateUrl: "views/contacts/details.html",
+        })
+
+    .state('app.financial', {
+            url: "/financial",
+            abstract: true,
+            templateUrl: "views/financial/_layout.html",
+            controller: "FinancialCtrl"
+        })
+        .state('app.financial.dashboard', {
+            url: "/",
+            templateUrl: "views/financial/dashboard.html",
+        })
+        .state('app.financial.details', {
+            url: "/{:fid}",
+            templateUrl: "views/financial/details.html",
+        })
 })
 
 .controller("AppCtrl", function($scope) {
-	$scope.topmenu = [{
-		"label": "Dashboard",
-		"sref": "app.dashboard",
-	}, {
-		"label": "Contacts",
-		"sref": "app.contacts.search",
-	}, {
-		"label": "Financial",
-		"sref": "app.financial.dashboard",
-	}];
+    $scope.topmenu = [{
+        "label": "Dashboard",
+        "sref": "app.dashboard",
+    }, {
+        "label": "Contacts",
+        "sref": "app.contacts.search",
+    }, {
+        "label": "Financial",
+        "sref": "app.financial.dashboard",
+    }];
 })
 
 .controller("DashboardCtrl", function($scope) {})
 
-.controller("ContactsCtrl", function($scope, $fakedb) {
+.controller("ContactsCtrl", function($scope) {
 
-	$fakedb.companies().then(function(companies) {
-		$scope.companies = companies;
-	})
+    $fakedb.companies().then(function(companies) {
+        $scope.companies = companies;
+    })
 
 })
 
 .controller("FinancialCtrl", function($scope) {})
 
-.factory("$fakedb", function($http) {
-	return {
-		companies: function() {
-			return $http
-				.get("data/database.json")
-				.then(function(response) {
-					if (response.error) {
-						console.log("Remote Data Error", response.data);
-					}
-					return response.data.Companies;
-				}, function(response) {
-					//console.log("response",response);
-					var errorStatus;
-					if (response.status === 0) {
-						errorStatus = 'Could not connect to server<br>Check your internet connectivity';
-					} else if (response.status == 401) {
-						errorStatus = 'Unauthorized';
-					} else if (response.status == 405) {
-						errorStatus = 'HTTP verb not supported [405]';
-					} else if (response.status == 500) {
-						errorStatus = 'Internal Server Error [500].';
-					} else {
-						errorStatus = JSON.parse(JSON.stringify(response.data.error));
-					}
+.controller("LoginCtrl", function($scope, $timeout) {
+    // $scope.loading = true;
+    $scope.LoginSubmit = function() {
+        $scope.loading = true;
+        $timeout(function() {
+            $scope.$state.go("app.dashboard")
+        }, 2000);
+    }
+})
 
-					return {
-						error: true,
-						data: errorStatus
-					};
-				});
-		}
-	}
+.factory("$fakedb", function($http) {
+    return {
+        companies: function() {
+            return $http
+                .get("data/database.json")
+                .then(function(response) {
+                    if (response.error) {
+                        console.log("Remote Data Error", response.data);
+                    }
+                    return response.data.Companies;
+                }, function(response) {
+                    //console.log("response",response);
+                    var errorStatus;
+                    if (response.status === 0) {
+                        errorStatus = 'Could not connect to server<br>Check your internet connectivity';
+                    } else if (response.status == 401) {
+                        errorStatus = 'Unauthorized';
+                    } else if (response.status == 405) {
+                        errorStatus = 'HTTP verb not supported [405]';
+                    } else if (response.status == 500) {
+                        errorStatus = 'Internal Server Error [500].';
+                    } else {
+                        errorStatus = JSON.parse(JSON.stringify(response.data.error));
+                    }
+
+                    return {
+                        error: true,
+                        data: errorStatus
+                    };
+                });
+        }
+    }
 })
 
 
