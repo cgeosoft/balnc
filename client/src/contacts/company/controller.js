142f301878cf87633b622dbd88b7cf3766fa3a35
Christos Georgiou <xris.georgiou@gmail.com>
2016-01-04 23:17:08 +0200

    add pdfmaker

--- client/src/contacts/company/controller.js
+++ client/src/contacts/company/controller.js
@@ -55,6 +55,24 @@
       route: 'app.contacts.company.files',
     }];
 
+    vm.Print = function() {
+      var docDefinition = {
+        content: 'ελλάδα Ά an sample PDF printed with pdfMakeThis is an sample PDF printed with pdfMakeThis is an sample PDF printed with pdfMake'
+      };
+      // or become pretty complex (having multi-level tables, images, lists, paragraphs, margins, styles etc...).
+      //
+      // As soon as you have the document-definition-object, you're ready to create and open/print/download the PDF:
+
+      // open the PDF in a new window
+      pdfMake.createPdf(docDefinition).open();
+
+      // print the PDF (temporarily Chrome-only)
+      // pdfMake.createPdf(docDefinition).print();
+
+      // download the PDF (temporarily Chrome-only)
+      // pdfMake.createPdf(docDefinition).download('optionalName.pdf');
+    }
+
     activate();
 
     function activate() {
