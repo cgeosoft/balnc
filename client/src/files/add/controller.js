fe5f7673ecf4c86ae913213f3e9769656f514069
Christos Georgiou <xris.georgiou@gmail.com>
2016-01-09 15:50:50 +0200

    fixed add files modal

--- client/src/files/add/controller.js
+++ client/src/files/add/controller.js
@@ -23,46 +23,46 @@
 
     var uploader = vm.uploader = new FileUploader({
       vm: vm, // to automatically update the html. Default: $rootScope
-      url: '/api/containers/'+vm.Container+'/upload',
-      formData: [{
-        key: 'value'
-      }]
+      url: '/api/containers/' + vm.Container + '/upload',
+      // formData: [{
+      //   key: 'value'
+      // }]
     });
 
     // ADDING FILTERS
-    uploader.filters.push({
-      name: 'filterName',
-      fn: function(item, options) { // second user filter
-        console.info('filter2');
-        return true;
-      }
-    });
+    // uploader.filters.push({
+    //   name: 'filterName',
+    //   fn: function(item, options) { // second user filter
+    //     console.info('filter2');
+    //     return true;
+    //   }
+    // });
 
     // REGISTER HANDLERS
     // --------------------
-    uploader.onAfterAddingFile = function(item) {
-      console.info('After adding a file', item);
-    };
-    // --------------------
-    uploader.onAfterAddingAll = function(items) {
-      console.info('After adding all files', items);
-    };
-    // --------------------
-    uploader.onWhenAddingFileFailed = function(item, filter, options) {
-      console.info('When adding a file failed', item);
-    };
-    // --------------------
-    uploader.onBeforeUploadItem = function(item) {
-      console.info('Before upload', item);
-    };
-    // --------------------
-    uploader.onProgressItem = function(item, progress) {
-      console.info('Progress: ' + progress, item);
-    };
-    // --------------------
-    uploader.onProgressAll = function(progress) {
-      console.info('Total progress: ' + progress);
-    };
+    // uploader.onAfterAddingFile = function(item) {
+    //   console.info('After adding a file', item);
+    // };
+    // // --------------------
+    // uploader.onAfterAddingAll = function(items) {
+    //   console.info('After adding all files', items);
+    // };
+    // // --------------------
+    // uploader.onWhenAddingFileFailed = function(item, filter, options) {
+    //   console.info('When adding a file failed', item);
+    // };
+    // // --------------------
+    // uploader.onBeforeUploadItem = function(item) {
+    //   console.info('Before upload', item);
+    // };
+    // // --------------------
+    // uploader.onProgressItem = function(item, progress) {
+    //   console.info('Progress: ' + progress, item);
+    // };
+    // // --------------------
+    // uploader.onProgressAll = function(progress) {
+    //   console.info('Total progress: ' + progress);
+    // };
     // --------------------
     // uploader.onSuccessItem = function(item, response, status, headers) {
     //   console.info('Success', response, status, headers);
@@ -70,20 +70,21 @@
     // };
     // --------------------
     uploader.onErrorItem = function(item, response, status, headers) {
-      console.info('Error', response, status, headers);
+      item.error = response.error;
+      // console.info('Error', response, status, headers);
     };
-    // --------------------
-    uploader.onCancelItem = function(item, response, status, headers) {
-      console.info('Cancel', response, status);
-    };
-    // --------------------
-    uploader.onCompleteItem = function(item, response, status, headers) {
-      console.info('Complete', response, status, headers);
-    };
-    // --------------------
-    uploader.onCompleteAll = function() {
-      console.info('Complete all');
-    };
+    // // --------------------
+    // uploader.onCancelItem = function(item, response, status, headers) {
+    //   console.info('Cancel', response, status);
+    // };
+    // // --------------------
+    // uploader.onCompleteItem = function(item, response, status, headers) {
+    //   console.info('Complete', response, status, headers);
+    // };
+    // // --------------------
+    // uploader.onCompleteAll = function() {
+    //   console.info('Complete all');
+    // };
 
   }
 
