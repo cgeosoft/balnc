5a0fd3d12deaf3ae1c6c92279489bbdd8e09d107
Christos Georgiou <Christos Georgiou>
2016-02-26 12:11:34 +0200

    minor clean up and dep updates

--- server/datasources.development.js
+++ server/datasources.development.js
@@ -1,9 +1,7 @@
-var config = require('../common/config/system/database.json');
-
 module.exports = {
   db: {
-    database: config.database,
-    username: (config.username != "ENV") ? config.username : process.env.MYSQL_USERNAME,
-    password: (config.password != "ENV") ? config.password : process.env.MYSQL_PASSWORD
+    database: "balance",
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD
   }
 };
