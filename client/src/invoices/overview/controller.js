3843df4fea3aa0dc636b3e22d4aecd8905496d1e
Christos Georgiou <xris.georgiou@gmail.com>
2016-05-22 17:06:09 +0300

    inv table

--- client/src/invoices/overview/controller.js
+++ client/src/invoices/overview/controller.js
@@ -6,33 +6,25 @@
     .controller("InvoicesOverviewController", InvoicesOverviewController);
 
   /* @ngInject */
-  function InvoicesOverviewController() {
+  function InvoicesOverviewController(Invoice) {
     var vm = this;
 
     activate();
 
     function activate() {
-      vm.news = _.sortBy([{
-        "date": "2015-01-01",
-        "content": "Tempor ut qui non mollit qui.",
-        "title": "ea enim veniam laboris ullamco",
-        "url": "http://google.com"
-      }, {
-        "date": "2015-01-01",
-        "content": "Officia id nulla nulla id ipsum minim.",
-        "title": "cillum aute deserunt labore est",
-        "url": "http://google.com"
-      }, {
-        "date": "2015-01-01",
-        "content": "Ullamco ut voluptate aliqua laboris nostrud.",
-        "title": "velit occaecat adipisicing tempor sit",
-        "url": "http://google.com"
-      }, {
-        "date": "2015-01-01",
-        "content": "Ipsum nisi non adipisicing ex aute veniam.",
-        "title": "non do ipsum et cillum",
-        "url": "http://google.com"
-      }], 'date');
+      Invoice.find({
+          filter: {
+            limit: 10,
+            order: "date DESC",
+          }
+        })
+        .$promise
+        .then(function(_invoices) {
+          vm.invoices = _invoices;
+        })
+        .finally(function() {
+
+        });
     }
 
 
