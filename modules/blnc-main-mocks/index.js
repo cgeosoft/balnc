2d8639ec20d6c49615938025f8dac02ab566b0e7
christos georgiou <georgiou.christos@outlook.com>
2017-08-23 13:20:14 +0300

    start faker project

---
+++ modules/blnc-main-mocks/index.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+var path = require('path');
+var jsf = require('json-schema-faker');
+var faker = require('faker');
+
+// let set = "business/contacts/order"
+let f = "order"
+let set = "business/orders/" + f
+let counter = 10
+
+let mockPath = path.join(__dirname, "../../schemas/" + set + ".json")
+console.log("Path:", mockPath)
+var schema = JSON.parse(fs.readFileSync(mockPath, "utf8"))
+console.log("File Data:", schema)
+
+jsf
+  .resolve({
+    type: "array",
+    items: schema
+  })
+  .then(function (sample) {
+    console.log("sample:", sample)
+    fs.writeFileSync("./mocks/" + f + ".json", JSON.stringify(sample, "", 4))
+  }, function (error) {
+    console.error("error:", error)
+  });
