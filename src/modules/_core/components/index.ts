4ec8036b2bc7643f933cfa8e989b05dce6ec1a45
Christos Georgiou <georgiou.christos@outlook.com>
2017-12-07 11:28:58 +0200

    fix filetype

---
+++ src/modules/_core/components/index.ts
@@ -0,0 +1,3 @@
+export { CoreComponent } from './core/core.component'
+export { MainComponent } from './main/main.component'
+export { PageNotFoundComponent } from './page-not-found/page-not-found.component'
