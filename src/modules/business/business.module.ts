6384c56197fb90be28afdeafe898e3a9ce5c926c
Christos Georgiou <georgiou.christos@outlook.com>
2019-02-10 18:53:45 +0200

    ui improv sidemenu

--- src/modules/business/business.module.ts
+++ src/modules/business/business.module.ts
@@ -4,13 +4,17 @@ import { CommonModule } from '@balnc/common'
 import { InvoicesModule } from './invoices/invoices.module'
 import { OrdersModule } from './orders/orders.module'
 import { ContactsModule } from './contacts/contacts.module'
+import { RouterModule } from '@angular/router'
 
 @NgModule({
   imports: [
     CommonModule,
     ContactsModule,
     InvoicesModule,
-    OrdersModule
+    OrdersModule,
+    RouterModule.forChild([
+      { path: '', redirectTo: 'contacts', pathMatch: 'full' }
+    ])
   ],
   declarations: [],
   providers: [],
