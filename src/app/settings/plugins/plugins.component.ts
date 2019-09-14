b99543e1a1ddfeef3a76231a0c9fd6ea7d5ad52e
Christos Georgiou <georgiou.christos@outlook.com>
2019-09-14 13:26:03 +0300

    add notes

--- src/app/settings/plugins/plugins.component.ts
+++ src/app/settings/plugins/plugins.component.ts
@@ -1,26 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { ConfigService } from '@balnc/core';
-import { Plugin, Profile } from '@balnc/shared';
+import { Component, OnInit } from '@angular/core'
+import { ConfigService } from '@balnc/core'
+import { Plugin, Profile } from '@balnc/shared'
 
 @Component({
   selector: 'app-settings-plugins',
-  templateUrl: './plugins.component.html'
+  templateUrl: './plugins.component.html',
+  styleUrls: ['plugins.component.scss']
 })
 export class PluginsComponent implements OnInit {
 
   plugins: Plugin[]
   profile: Profile
 
-  constructor(
+  constructor (
     private configService: ConfigService
   ) { }
 
-  ngOnInit() {
+  ngOnInit () {
     this.profile = this.configService.profile
     this.plugins = this.configService.plugins
   }
-  
-  switchStatus(pluginId: string) {
+
+  switchStatus (pluginId: string) {
     this.profile.plugins[pluginId] = !this.profile.plugins[pluginId]
     this.configService.saveProfile(this.profile)
   }
