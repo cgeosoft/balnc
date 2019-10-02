8606bc6f565b0c92f5863d349b817b6f1ee4fc5f
Christos Georgiou <georgiou.christos@outlook.com>
2019-10-02 10:25:11 +0300

    Update app.component.ts

--- packages/app/src/app/app.component.ts
+++ packages/app/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  template: `<router-outlet></router-outlet>`
+  template: `<router-outlet></router-outlet><script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="5978eab8-71b3-413d-9963-67a946a97ca5";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>`
 })
 export class AppComponent {
 }
