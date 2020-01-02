1629e427c20723e7e2aefb4467d269e356a69e8c
Christos Georgiou <georgiou.christos@outlook.com>
2020-01-02 23:06:30 +0200

    fix layout

--- packages/app/src/app/business/contacts/contacts/contacts.component.ts
+++ packages/app/src/app/business/contacts/contacts/contacts.component.ts
@@ -21,9 +21,10 @@ export class ContactsComponent implements OnInit {
       { label: 'Name', style: { 'min-width': '300px' }, template: 't1', locked: true },
       { label: 'Phones', style: { width: '150px' }, val: (item: Contact) => item.phones },
       { label: 'Emails', style: { width: '200px' }, val: (item: Contact) => item.emails },
-      { label: 'Socials', val: (item: Contact) => item.socials, hidden: true },
-      { label: 'Offices', val: (item: Contact) => item.offices, hidden: true },
-      { label: 'TaxDetails', val: (item: Contact) => (item.taxDetails) ? item.taxDetails.taxOffice : '-', hidden: true }
+      { label: 'Tags', style: { width: '200px' }, val: (item: Contact) => item.tags.join(', '), hidden: true },
+      { label: 'Socials', style: { width: '200px' }, val: (item: Contact) => item.socials, hidden: true },
+      { label: 'Offices', style: { width: '200px' }, val: (item: Contact) => item.offices, hidden: true },
+      { label: 'TaxDetails', style: { width: '200px' }, val: (item: Contact) => (item.taxDetails) ? item.taxDetails.taxOffice : '-', hidden: true }
     ]
   }
 
