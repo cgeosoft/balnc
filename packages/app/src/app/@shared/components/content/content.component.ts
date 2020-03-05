afef9f295913530c032ecc67b1bf27a48331a5de
Christos Georgiou <georgiou.christos@outlook.com>
2020-03-05 09:33:26 +0200

    refactor app-content

--- packages/app/src/app/@shared/components/content/content.component.ts
+++ packages/app/src/app/@shared/components/content/content.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['content.component.scss']
 })
-export class ContentComponent {}
+export class ContentComponent {
+  @Input() box = false
+}
