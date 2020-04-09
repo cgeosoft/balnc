3996860e50ef94aae2189c45b17135233044c2a3
Christos Georgiou <georgiou.christos@outlook.com>
2020-04-10 00:51:42 +0300

    update server

---
+++ packages/server/src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import PouchDB from 'pouchdb';
+
+const expressPouchdb = require("express-pouchdb");
+const app = express();
+
+const pouchDB = expressPouchdb(PouchDB.defaults({
+    prefix: './data/'
+}))
+
+app.use('/db', pouchDB);
+
+app.get("/", (req, res) => {
+    res.send("Hello World2")
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running in http://localhost:${PORT}`)
+})
