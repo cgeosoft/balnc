849d140e1a77ddf0cd355487547b68b5d6cf003f
Christos Georgiou <georgiou.christos@outlook.com>
2020-04-15 00:56:42 +0300

    update logger

--- packages/server/src/commons/logger.ts
+++ packages/server/src/commons/logger.ts
@@ -6,13 +6,10 @@ export const logger = winston.createLogger({
     format: winston.format.json(),
     defaultMeta: { service: 'user-service' },
     transports: [
-        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'logs/combined.log' })
+        new winston.transports.File({ filename: './config/error.log', level: 'error' }),
+        new winston.transports.File({ filename: './config/combined.log' }),
+        new winston.transports.Console({
+            format: winston.format.simple()
+        })
     ]
 });
-
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-        format: winston.format.simple()
-    }));
-}
