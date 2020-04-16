e28872d7c1871ac6ccdbd2ca77792c6d8fb47674
Christos Georgiou <georgiou.christos@outlook.com>
2020-04-17 01:00:14 +0300

    fix support shell

--- packages/app/src/app/support/@shell/shell.component.ts
+++ packages/app/src/app/support/@shell/shell.component.ts
@@ -1,39 +1,28 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
+import { MenuItem } from '@balnc/shared'
 
 @Component({
   selector: 'app-support-shell',
   templateUrl: './shell.component.html'
 })
-export class ShellComponent implements OnInit {
+export class ShellComponent {
 
-  sidebar = {
-    title: 'Support',
-    menu: [{
-      url: '/support/faq',
-      icon: ['far', 'question-circle'],
-      type: 'button',
-      label: 'FAQ'
-    },
-    {
-      url: '/support/live',
-      icon: 'concierge-bell',
-      type: 'button',
-      label: 'Live Chat'
-    },
-    {
-      url: '/support/about',
-      icon: 'info-circle',
-      type: 'button',
-      label: 'About'
-    }]
-  }
-
-  constructor () {
-    // todo
-  }
-
-  ngOnInit () {
-    // todo
-  }
-
+  menu: MenuItem[] = [{
+    route: '/support/faq',
+    icon: ['far', 'question-circle'],
+    type: 'button',
+    label: 'FAQ'
+  },
+  {
+    route: '/support/live',
+    icon: 'concierge-bell',
+    type: 'button',
+    label: 'Live Chat'
+  },
+  {
+    route: '/support/about',
+    icon: 'info-circle',
+    type: 'button',
+    label: 'About'
+  }]
 }
