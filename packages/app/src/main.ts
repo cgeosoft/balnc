0967524b4bc3e27b0f5cf8c3312e743aca4765b5
Christos Georgiou <georgiou.christos@outlook.com>
2020-04-21 10:16:49 +0300

    add sentry env

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -2,11 +2,12 @@ import { enableProdMode } from '@angular/core'
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import * as Sentry from '@sentry/browser'
 import { AppModule } from './app/app.module'
-import { environment } from './environments/environment'
+import { environment } from './environments/environment.prod'
 
 Sentry.init({
   release: `${environment.version}-${environment.build.git.hash}`,
-  dsn: 'https://9710c0dc35d14a62a8725d354e9e915e@sentry.io/3249691',
+  environment: environment.production ? 'production' : 'development',
+  dsn: 'https://9710c0dc35d14a62a8725d354e9e915e@sentry.io/3249691'
 })
 
 if (environment.production) {
