52a5c90092934a76201b6f2a1dfafa63f3cd1c80
Christos Georgiou <georgiou.christos@outlook.com>
2020-04-23 19:12:23 +0300

    fix server build to production

--- packages/server/src/build.ts
+++ packages/server/src/build.ts
@@ -1,9 +1,9 @@
 // this is generated from scripts/prebuild.js
 export const build = {
-  builded: 1587461147840,
+  builded: 1587658308241,
   author: "Christos Georgiou - georgiou.christos@outlook.com",
-  commited: 1587453653000,
-  message: "update packages",
-  hash: "233baf16fd9cdcc588188adddce525a96c6752b5",
-  branch: "develop",
+  commited: 1587658115000,
+  message: "better relase",
+  hash: "efff91d4e005800a79abd75d4655b7a61aeb2677",
+  branch: "master",
 }
