4c80a9f57317cd56682b999cbffe4b25deda572b
Christos Georgiou <georgiou.christos@outlook.com>
2020-06-14 13:40:38 +0300

    [app] add: product models

--- packages/app/src/app/products/@shared/product.ts
+++ packages/app/src/app/products/@shared/product.ts
@@ -2,4 +2,35 @@ import { Entity } from '@balnc/core';
 
 export interface Product extends Entity {
   name: string
+  key: string
+  attributes: { [key: string]: string | number }[],
+  description?: string,
+  images: [],
+  statement?: string,
+  type: 'service' | 'good',
+  shippable: boolean
+  unitLabel?: string
+}
+
+// using _group for product
+export interface SKU extends Entity {
+  name: string
+  key: string
+  attributes: { [key: string]: string | number }[],
+  description: string,
+  images: [],
+  currency: string,
+  reservation: {
+    expression: string
+  }
+  inventory: {
+    quantity: number,
+    type: 'finite' | 'bucket' | 'infinite',
+    value?: 'in_stock' | 'limited' | 'out_of_stock'
+  },
+}
+
+export interface InventoryRecord extends Entity {
+  amount: number
+  description: string
 }
